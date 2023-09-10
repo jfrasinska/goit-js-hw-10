a7d62f4d7d9c2946243b088ab61579ec382c0078
jfrasinska <j.frasinska@gmail.com>
2023-09-10 21:19:34 +0200

    adding index htm and js files index and cat-api

---
+++ src/cat-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_ZpsxYaY2I9CRaYfoTH4mKs5B5ZVSbbu4jcYjaJRDqrKXQm63Xj2vDeSzi65RrFFH';
+
+// Funkcja do pobierania kolekcji ras z API
+export function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Błąd podczas pobierania ras:', error);
+      throw error;
+    });
+}
+
+// Funkcja do pobierania informacji o kocie na podstawie identyfikatora rasy
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data[0])
+    .catch(error => {
+      console.error('Błąd podczas pobierania informacji o kocie:', error);
+      throw error;
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import './css/styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const DEBOUNCE_DELAY = 300;
+document.addEventListener('DOMContentLoaded', function () {
+  const breedSelect = document.querySelector('.breed-select');
+  const loader = document.querySelector('.loader');
+  const error = document.querySelector('.error');
+  const catInfo = document.querySelector('.cat-info');
+
+  breedSelect.addEventListener('change', function () {
+    const selectedBreed = breedSelect.value;
+
+    loader.style.display = 'block';
+    error.style.display = 'none';
+
+    fetchCatByBreed(selectedBreed)
+      .then(cat => {
+        loader.style.display = 'none';
+
+        if (cat && cat.url) {
+          catInfo.innerHTML = `<img src="${cat.url}" alt="Kot" />`;
+        } else {
+          catInfo.innerHTML = 'Nie znaleziono zdjęć kota tej rasy.';
+        }
+      })
+      .catch(error => {
+        loader.style.display = 'none';
+        error.style.display = 'block';
+        console.error('Wystąpił błąd:', error);
+      });
+  });
+
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      console.error('Błąd podczas pobierania ras:', error);
+    });
+});
