448670a9800027b86ed56a837b1991d61bf96678
jfrasinska <j.frasinska@gmail.com>
2023-09-18 07:18:50 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,51 +1,89 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import axios from "axios";
+import Notiflix from "notiflix";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const breedSelect = document.querySelector('.breed-select');
-  const loader = document.querySelector('.spinner');
-  const error = document.querySelector('.error');
-  const catInfo = document.querySelector('.cat-info');
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "39497508-491be661073b62dac12b07e34";
 
-  breedSelect.addEventListener('change', function () {
-    const selectedBreed = breedSelect.value;
+const form = document.getElementById("search-form");
+const input = form.querySelector('input[name="searchQuery"]');
+const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
 
-    loader.style.display = 'block';
-    breedSelect.style.display = 'none';
-    error.style.display = 'none';
+let page = 1;
+let currentSearch = "";
 
-    fetchCatByBreed(selectedBreed)
-      .then(cat => {
-        loader.style.display = 'none';
-        breedSelect.style.display = 'block';
-        if (cat && cat.url) {
-          const catHTML = `
-      <img src="${cat.url}" alt="Kot" />
-      <h2>Breed name: ${cat.breedInfo.name}</h2>
-      <p>${cat.breedInfo.description}</p>
-      <p>Temperament: ${cat.breedInfo.temperament}</p>
-    `;
-          catInfo.innerHTML = catHTML;
-        } else {
-          catInfo.innerHTML = 'Nie znaleziono zdjęć kota tej rasy.';
-        }
-      })
-      .catch(error => {
-        loader.style.display = 'none';
-        error.style.display = 'block';
-        console.error('Wystąpił błąd:', error);
-      });
-  });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const query = input.value.trim();
+  if (query === "") {
+    Notiflix.Notify.failure("Please enter a search keyword.");
+    return;
+  }
 
-  fetchBreeds()
-    .then(breeds => {
-      breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-      });
-    })
-    .catch(error => {
-      console.error('Błąd podczas pobierania ras:', error);
-    });
+  currentSearch = query;
+  page = 1;
+  gallery.innerHTML = "";
+  loadMoreBtn.style.display = "none";
+
+  searchImages(query);
 });
+
+loadMoreBtn.addEventListener("click", () => {
+  page += 1;
+  searchImages(currentSearch, page);
+});
+
+async function searchImages(query, page = 1) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: "true",
+    page: page,
+    per_page: 40,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    const data = response.data;
+
+    if (data.hits.length === 0) {
+      Notiflix.Notify.warning(
+        "Sorry, there are no images matching your search query. Please try again."
+      );
+      return;
+    }
+
+    createGallery(data.hits);
+
+    if (data.totalHits > page * 40) {
+      loadMoreBtn.style.display = "block";
+    } else {
+      loadMoreBtn.style.display = "none";
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    Notiflix.Notify.failure("An error occurred while fetching images.");
+  }
+}
+
+function createGallery(images) {
+  images.forEach((image) => {
+    const card = document.createElement("div");
+    card.classList.add("photo-card");
+    card.innerHTML = `
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+                <p class="info-item"><b>Views:</b> ${image.views}</p>
+                <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+                <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+            </div>
+        `;
+    gallery.appendChild(card);
+  });
+}
